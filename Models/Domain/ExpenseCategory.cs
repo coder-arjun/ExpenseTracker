466294f4466c4728406bfc33616db66e20e1a172
Coder-Arjun <aryanarju26@gmail.com>
2026-03-03 21:03:08 +0530

    Added two more categories

--- Models/Domain/ExpenseCategory.cs
+++ Models/Domain/ExpenseCategory.cs
@@ -8,7 +8,10 @@
         Bills = 3,
         Shopping = 4,
         Entertainment = 5,
-        Other = 6
+        Other = 6,
+        Tea = 7,
+        Vehicle = 8,
+        Marriage = 9
     }
 
 }
